455d0764fa6f00b2b4a48b4cbf90c03b77d1e1f8
ehob <safonov.andrey.eho@gmail.com>
2019-05-26 23:21:02 +0200

    Add nodemon

--- server/index.js
+++ server/index.js
@@ -10,15 +10,16 @@ passport.use(
         clientID: keys.googleClintID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, accessToken => {
-        console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken);
+        console.log('refreshToken', refreshToken);
+        console.log('profile', profile);
+        console.log('done', done);
     })
 );
 
-app.get(
-    '/auth/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-);
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
